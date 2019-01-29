79d4d473ab85c92bfddb58e5b2df911030f3dda3
Olivier Cortès <olive@cocoliv.es>
2019-01-29 10:41:15 +0100

    More GTK imports.

--- bibed/gui/gtk.py
+++ bibed/gui/gtk.py
@@ -1,6 +1,15 @@
 import gi
 gi.require_version('Gtk', '3.0')
 gi.require_version('Gdk', '3.0')
-gi.require_version("Notify", "0.7")
+gi.require_version('Notify', '0.7')
 
-from gi.repository import GLib, Gio, Gtk, Gdk, GdkPixbuf, Pango, Notify  # NOQA
+from gi.repository import (  # NOQA
+    GObject,
+    GLib,
+    Gio,
+    Gtk,
+    Gdk,
+    GdkPixbuf,
+    Pango,
+    Notify
+)
